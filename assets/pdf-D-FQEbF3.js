b67a0af259c305c9555cc151a14ffd7ab8b39bfa
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2026-03-03 15:25:10 +0600

    Updates

--- assets/pdf-D-FQEbF3.js
+++ assets/pdf-D-FQEbF3.js
@@ -1,4 +1,4 @@
-import{E as Qn}from"./pdf-CGvDKC3d.js";import{g as gn}from"./index-DIiTJie-.js";/*!
+import{E as Qn}from"./pdf-DEVKxTfB.js";import{g as gn}from"./index-DGCFt-yy.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
